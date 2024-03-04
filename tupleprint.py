2a2a655db8d0750ebf52ca179d18145af0c2f8c8
Bhuvan.R <144478466+Thor-asgardian@users.noreply.github.com>
2024-03-04 22:50:34 +0530

    Create tupleprint.py

---
+++ tupleprint.py
@@ -0,0 +1,15 @@
+# Create a tuple
+thistuple = ("apple", "banana", "cherry")
+
+# Print the tuple
+print(thistuple)
+
+# Print the first item of the tuple
+print(thistuple[0])
+
+# Print the last item of the tuple
+print(thistuple[-1])
+
+# Print the items of the tuple using a for loop
+for x in thistuple:
+  print(x)
