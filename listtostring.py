310d52118cadaac339971a3ad42d7ed149ff6655
Bhuvan.R <144478466+Thor-asgardian@users.noreply.github.com>
2024-03-04 22:52:06 +0530

    Create listtostring.py

---
+++ listtostring.py
@@ -0,0 +1,11 @@
+def convert_list_to_string(list1):
+    string = ""
+    for item in list1:
+        string += item
+    return string
+
+
+# Driver code
+list1 = ["h", "e", "l", "l", "o"]
+string = convert_list_to_string(list1)
+print(string)
